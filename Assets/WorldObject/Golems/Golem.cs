9f7330555399e6dd3e3cb16f4bf2bf5a1253bb26
Erik Mansén <erima668@student.liu.se>
2017-12-07 20:40:05 +0100

    Add scripts and prefabs for the different golem types, also make golems hold ore in right hand

---
+++ Assets/WorldObject/Golems/Golem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Unit {
+
+    protected override void PickUp(Ore ore) {
+        if (carrying) {
+            Drop();
+        }
+
+        Vector3 attachPos = transform.Find("RightHand").position;
+        ore.transform.position = attachPos;
+        ore.transform.parent = transform;
+
+        ore.OnPickUp(this);
+        carrying = true;
+    }
+}
